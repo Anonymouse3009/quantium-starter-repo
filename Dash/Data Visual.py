0e238b4626bb3f3275eed408c7b0956605e7d712
Aroosh <127726015+Anonymouse3009@users.noreply.github.com>
2023-06-27 12:15:02 +0400

    Add files via upload
    Dash visualization

---
+++ Dash/Data Visual.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from dash import Dash,dcc,html
+import plotly.express as p
+
+app = Dash(__name__)
+
+source = 'Daily_sales_Data_Formatted.csv'
+file = pd.read_csv(source)
+file = file.sort_values(by ="Date")
+
+header = html.H1(
+    "Pink Morsel Sales",
+    id="header"
+)
+
+fig = p.bar(file,x="Date",y="Sales")
+visual = dcc.Graph(
+    id = "graph",
+    figure= fig
+)
+app.layout = html.Div(
+    [
+        header, visual
+    ]
+)
+if __name__ == '__main__':
+    app.run_server(debug=True)
