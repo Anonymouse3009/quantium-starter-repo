777573f95c67c0952138173caf7f884e26434ae7
Aroosh <127726015+Anonymouse3009@users.noreply.github.com>
2023-07-02 11:52:35 +0400

    Update Data Visual.py
    Data visual:
    Region picker, added colours to elements

--- Dash/Data Visual.py
+++ Dash/Data Visual.py
@@ -1,26 +1,66 @@
 import pandas as pd
 from dash import Dash,dcc,html
 import plotly.express as p
+from plotly.express import line
+from dash import Input,Output
 
-app = Dash(__name__)
 
-source = 'Daily_sales_Data_Formatted.csv'
+source = 'Dash\Daily_sales_Data_Formatted.csv'
 file = pd.read_csv(source)
 file = file.sort_values(by ="Date")
 
+
+app = Dash(__name__)
+
+
 header = html.H1(
     "Pink Morsel Sales",
-    id="header"
+    id="header",
+    style={
+        'textAlign' : 'center'
+        
+    }
 )
+def createGraph(file):
+    chart = line(file,x="Date",y="Sales")
+    chart.update_layout(
+        plot_bgcolor = "#d0e0e3",
+        paper_bgcolor = "#d9ead3"
 
-fig = p.line(file,x="Date",y="Sales")
-visual = dcc.Graph(
-    id = "graph",
-    figure= fig
+    )
+    return chart
+
+graph = dcc.Graph(
+        id= "lineGraph",
+        figure=createGraph(file)
+    )
+
+
+pick_region = dcc.RadioItems(
+    ["north","south","east","west","all"],
+    id= "region", inline=True,
+    style={
+        'color': 'DarkBlue', 'font_size': 50
+    }
 )
+@app.callback(
+    Output(graph,"figure"),
+    Input(pick_region,"value")
+    
+    
+)
+def newgraph(user):
+    if user == "all": 
+        newdata = file
+    else:
+        newdata = file[file["Region"] == user]
+    figure = createGraph(newdata)
+    return figure
+
+
 app.layout = html.Div(
     [
-        header, visual
+        header, graph, pick_region
     ]
 )
 if __name__ == '__main__':
